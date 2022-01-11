7859eccba28d9d0d1881a724dc90180533aba463
MehdiMiah <Mehdimiah@hotmail.co.uk>
2022-01-11 11:42:15 +0000

    add title change in relation to die value

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-console.log("images/dice" + randomNumber1 + ".png")
+
 
 document.querySelectorAll(".img1")[0].setAttribute("src","images/dice" + randomNumber1 +".png");
 document.querySelectorAll(".img2")[0].setAttribute("src","images/dice" + randomNumber2 +".png");
+
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerText = "Player 1 Wins! 🚩"
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerText = "🚩 Player 2 Wins! "
+} else {
+    document.querySelector("h1").innerText = " Draw! "
+}
